9a33c797faed4eea85e764bf739606e5c7747772
matts207 <36020380+matts207@users.noreply.github.com>
2018-12-14 11:11:33 -0500

    Updated

--- code_enforcement/code_enforcement_1.py
+++ code_enforcement/code_enforcement_1.py
@@ -1,41 +1,11 @@
 import requests, json
-from datetime import datetime, timedelta
-import pprint
 from bs4 import BeautifulSoup
 
 
 google_directions_api_url = 'https://maps.googleapis.com/maps/api/directions/json?origin={O}&destination={D}&waypoints=optimize:true|{W}&key={K}'
 
-
 directions_key = 'AIzaSyAYixVeTbunLmxgrk3TcVrUE9sWhGY64bI'
 
-
-pp = pprint.PrettyPrinter(indent=2)
-
-o = "207 Peak Dr. 15090"
-
-locations = {"1000 Ross Park Mall Dr, Pittsburgh, PA 15237": ['None', '120'],
-            '1750 Clairton Rd, West Mifflin, PA 15122': ['None', '120'],
-            '|144 Ashley Hill Dr. 15090': ['None', '120'],
-            '125 Monroeville Ave, Turtle Creek, PA 15145': ['None', '160']}
-# Dict that will eventually have weather or not there is a set time and
-# the estimated time needed at each destination
-
-
-list_of_stops = ["Pittsburgh, PA", "CCAC South"]
-waypoints = ''
-
-for key, val in locations.items():
-    if val[0] == 'None':
-        waypoints += key
-        waypoints += '|'
-# Sets up the waypoints in correct format from locations dict.
-
-
-stops_w_set_times = {"8701 Perry Hwy, Pittsburgh, PA 15237": 1543357287}
-
-
-stops = {}
 address_file = 'addresses.json'
 
 
